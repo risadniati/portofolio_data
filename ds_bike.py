3395a467865aa635e33d677e81596a6cbeaccb96
risadniati <152244838+risadniati@users.noreply.github.com>
2023-11-29 16:38:03 +0700

    Update ds_bike.py

--- ds_bike.py
+++ ds_bike.py
@@ -2,7 +2,7 @@
 
 ## Import seluruh library yang diperlukan 
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns 
 import streamlit as st
 
